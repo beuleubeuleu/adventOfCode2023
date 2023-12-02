bae41c15060b7fa6775e09d24c6a97c36c10ea3f
beuleu <billyklotz789@gmail.com>
2023-12-02 15:08:05 +0100

    Part 1 resolve method

--- src/main/java/com/adventofcode/Day2/ProblemResolver.java
+++ src/main/java/com/adventofcode/Day2/ProblemResolver.java
@@ -16,16 +16,27 @@ public class ProblemResolver {
   public static boolean isPossible(Game testGame) {
     for (int i = 0; i < testGame.handfullsOfCubes.length; i++) {
       Map<String, Integer> handfull = testGame.handfullsOfCubes[i];
-      int handfullRed = (handfull.get("red")==null?0:handfull.get("red"));
-      int handfullBlue = (handfull.get("blue")==null?0:handfull.get("blue"));
-      int handfullGreen = (handfull.get("green")==null?0:handfull.get("green"));
+      int handfullRed = (handfull.get("red") == null ? 0 : handfull.get("red"));
+      int handfullBlue =
+        (handfull.get("blue") == null ? 0 : handfull.get("blue"));
+      int handfullGreen =
+        (handfull.get("green") == null ? 0 : handfull.get("green"));
 
-
-      if (handfullRed > assumption.get("red")) return false;      
+      if (handfullRed > assumption.get("red")) return false;
       if (handfullBlue > assumption.get("blue")) return false;
       if (handfullGreen > assumption.get("green")) return false;
-
     }
     return true;
+  }
+
+  public static int resolvePart1(String[] documentLines) {
+    int total=0;
+    for (String line : documentLines) {
+      Game game = DocumentParser.getGameFromLine(line);
+      if(isPossible(game)){
+        total += game.id;
+      }
+    }
+    return total;
   }
 }
