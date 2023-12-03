f385400f8475d4ebb15316850d8bdf685680740d
beuleu <billyklotz789@gmail.com>
2023-12-03 13:17:30 +0100

    example working, not off, need more tests

--- src/main/java/com/adventofcode/Day3/GondolaLift.java
+++ src/main/java/com/adventofcode/Day3/GondolaLift.java
@@ -1,11 +1,138 @@
 package com.adventofcode.Day3;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 public class GondolaLift {
 
-    public String[] EngineSchematics;
+  public String[] EngineSchematics;
 
-    public GondolaLift(String[] exampleDocs) {
-        this.EngineSchematics = exampleDocs;
+  public GondolaLift(String[] exampleDocs) {
+    this.EngineSchematics = exampleDocs;
+  }
+
+  public int[] getParts() {
+    List<Integer> numbersList = new ArrayList<>();
+    String[] inputArray = this.EngineSchematics;
+
+    for (int lineIndex = 0; lineIndex < inputArray.length; lineIndex++) {
+      String currentLine = inputArray[lineIndex];
+      Set<Integer> processedIndices = new HashSet<>();
+
+      for (int charIndex = 0; charIndex < currentLine.length(); charIndex++) {
+        if (processedIndices.contains(charIndex)) {
+          continue;
+        }
+
+        char currentChar = currentLine.charAt(charIndex);
+
+        if (Character.isDigit(currentChar)) {
+          int wholeNumber = extractNumber(inputArray, lineIndex, charIndex);
+          if(hasSymbolAdjacentChar(inputArray, lineIndex, charIndex, wholeNumber)){
+              numbersList.add(wholeNumber);
+          }
+
+          // Mark the indices of the processed characters for the current number
+          for (
+            int i = charIndex;
+            i < charIndex + String.valueOf(wholeNumber).length();
+            i++
+          ) {
+            processedIndices.add(i);
+          }
+        }
+      }
     }
 
+    System.out.println(IntStream.of(convertListToArray(numbersList)).sum());
+
+    return convertListToArray(numbersList);
+  }
+
+  private static boolean hasSymbolAdjacentChar(
+    String[] inputArray,
+    int lineIndex,
+    int charIndex,
+    int wholeNumber
+  ) {
+    String adjacent = "";
+
+    String currentLine = inputArray[lineIndex];
+    char charAfterWholeNumber = (
+        charIndex + String.valueOf(wholeNumber).length() < currentLine.length()
+      )
+      ? currentLine.charAt(charIndex + String.valueOf(wholeNumber).length())
+      : '\0';
+
+    char charBeforeWholeNumber = (charIndex - 1 >= 0)
+      ? currentLine.charAt(charIndex - 1)
+      : '\0';
+
+    String charAboveWholeNumber = (lineIndex - 1 >= 0)
+      ? inputArray[lineIndex - 1].substring(
+          Math.max(charIndex - 1, 0),
+          Math.min(
+            charIndex + String.valueOf(wholeNumber).length() + 1,
+            inputArray[lineIndex - 1].length()
+          )
+        )
+      : "";
+
+    String charBelowWholeNumber = (lineIndex + 1 < inputArray.length)
+      ? inputArray[lineIndex + 1].substring(
+          Math.max(charIndex - 1, 0),
+          Math.min(
+            charIndex + String.valueOf(wholeNumber).length() + 1,
+            inputArray[lineIndex + 1].length()
+          )
+        )
+      : "";
+
+    adjacent += charAboveWholeNumber;
+    adjacent += charBeforeWholeNumber;
+    adjacent += charAfterWholeNumber;
+    adjacent += charBelowWholeNumber;
+
+    return containsSymbols(adjacent);
+  }
+
+  private static int extractNumber(String[] inputArray, int row, int col) {
+    int currentNumber = 0;
+
+    // Accumulate digits to form the number
+    while (
+      col < inputArray[row].length() &&
+      Character.isDigit(inputArray[row].charAt(col))
+    ) {
+      currentNumber =
+        currentNumber *
+        10 +
+        Character.getNumericValue(inputArray[row].charAt(col));
+      col++;
+    }
+
+    return currentNumber;
+  }
+
+  private static int[] convertListToArray(List<Integer> numbersList) {
+    return numbersList.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  private static boolean containsSymbols(String input) {
+    // Define the set of allowed characters
+    String allowedCharacters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.";
+
+    // Check if the input contains any character not in the allowed set
+    for (char c : input.toCharArray()) {
+      if (allowedCharacters.indexOf(c) == -1) {
+        return true; // The string contains symbols
+      }
+    }
+
+    return false; // The string doesn't contain symbols
+  }
 }
